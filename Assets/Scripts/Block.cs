b1af481bfaaa7a1150a2d17b3ca4c59a5a404430
Jake Hall <jakethomashall@gmail.com>
2018-08-23 17:32:59 +0100

    First commit.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    public float speed = 6.5f;
+    BlockSpawner blockSpawner;
+    float screenWidthLocal;
+
+    void Start()
+    {
+        blockSpawner = FindObjectOfType<BlockSpawner>();
+        screenWidthLocal = Camera.main.orthographicSize;
+    }
+
+    void Update () {
+        BlockMovement();
+	}
+
+    void BlockMovement ()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime, Space.Self);
+        //screenWidthLocal + blockSpawner.blockSize
+        if (transform.position.y <= -screenWidthLocal - blockSpawner.blockSize)
+        {
+            Destroy(gameObject);
+            blockSpawner.count++;
+            print("Destroyed");
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 8.5f;
+    float screenHalfWidthInWorldUnits;
+    float halfPlayerWidth;
+
+
+	void Start () {
+        halfPlayerWidth = transform.localScale.x / 2f;
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize;
+	}
+	
+	
+	void Update () {
+        PlayerMovement();
+	}
+
+    void PlayerMovement ()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float velocity = inputX * speed;
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+        if (transform.position.x < -screenHalfWidthInWorldUnits - halfPlayerWidth)
+        {
+            transform.position = new Vector2(screenHalfWidthInWorldUnits - halfPlayerWidth, transform.position.y);
+        }
+        if (transform.position.x > screenHalfWidthInWorldUnits + halfPlayerWidth)
+        {
+            transform.position = new Vector2(-screenHalfWidthInWorldUnits + halfPlayerWidth, transform.position.y);
+        }
+    }
+}
